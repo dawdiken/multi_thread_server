d2e3fb13ced7c776d25a63fed8ef76031159c9d7
David <dawdiken@gmail.com>
2017-10-17 14:05:13 +0100

    hope this is correct

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server(9000);
+        new Thread(server).start();
+
+        try {
+            Thread.sleep(20 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Stopping Server");
+        server.stop();
+    }
+}
